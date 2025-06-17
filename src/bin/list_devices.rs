60873a0d2599b558159db55e54a93ffcbed25a1f
Timo Friedl <timo@babelforce.com>
2025-06-17 15:23:00 +0200

    chore: release 0.2.0

--- src/bin/list_devices.rs
+++ src/bin/list_devices.rs
@@ -1,4 +1,6 @@
+use cpal::{SampleRate, StreamConfig};
 use cpal::traits::{DeviceTrait, HostTrait};
+use tracing::{error, trace};
 
 pub fn main() -> anyhow::Result<()> {
     let host = cpal::default_host();
@@ -20,6 +22,32 @@ pub fn main() -> anyhow::Result<()> {
             }
         }
     }
+
+    // try to get default input device
+    let device = host
+        .default_input_device()
+        .expect("No input device available");
+
+    let config = StreamConfig {
+        channels: 1,
+        sample_rate: SampleRate(24_000),
+        buffer_size: cpal::BufferSize::Default,
+    };
+
+    let err_fn = |err| eprintln!("Input stream error: {}", err);
+
+    let _ = device.build_input_stream(
+        &config,
+        move |data: &[f32], _: &cpal::InputCallbackInfo| {
+            for frame in data.chunks(1) {
+                for &sample in frame {
+                    trace!("{:?}", sample)
+                }
+            }
+        },
+        err_fn,
+        None,
+    )?;
 
     Ok(())
 }
